e6e6be9c9e48e6b9d0e0bcf25a25ed42d3e879bd
Brayan Muñoz <balexander.munoz@udea.edu.co>
2023-06-08 12:34:08 -0500

    :wrench: Modify config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'B. Alexander',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Brayan Muñoz is a physicist, data scientist and software developer.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Brayan Muñoz',
+        short_name: 'Brayan Muñoz',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
