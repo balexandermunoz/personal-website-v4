da5b8bdbd8de3398a0e44b909ea43a6745aeb98d
Brayan Muñoz <balexander.munoz@udea.edu.co>
2023-06-08 18:06:15 -0500

    :memo: Added resume and updated href HARD CODE

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="personal-website-v4/resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
